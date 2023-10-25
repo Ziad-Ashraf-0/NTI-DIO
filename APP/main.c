e80d7ac09904cf067b8acf80627cb60130bc6b7a
Ziad Ashraf <zashraf011@gmail.com>
2023-10-25 15:08:06 +0300

    LCD-Keypad assignment finished

--- APP/main.c
+++ APP/main.c
@@ -35,69 +35,69 @@ int main(void) {
 	//H_LCD_void_displayCustomChar(0);
 	
 	
-	 // Display a message to enter the ID
-	 H_LCD_void_sendString("Enter ID: ");
+	// Display a message to enter the ID
+	H_LCD_void_sendString("Enter ID: ");
 
-	 // Get the user's input for ID using the keypad
-	 for (u8 i = 0; i < 4; i++) {
-		 u8 key = KEYPAD_getPressedKey();
-		 H_LCD_void_sendIntNum(key);
-		 enteredID = enteredID * 10 + key;
-	 }
-	 // Clear the LCD screen
-	 H_LCD_void_clearScreen();
+	// Get the user's input for ID using the keypad
+	for (u8 i = 0; i < 4; i++) {
+		u8 key = KEYPAD_getPressedKey();
+		H_LCD_void_sendIntNum(key);
+		enteredID = enteredID * 10 + key;
+	}
+	// Clear the LCD screen
+	H_LCD_void_clearScreen();
 
-	 // Display a message to enter the password
-	 H_LCD_void_sendString("Enter Password: ");
+	// Display a message to enter the password
+	H_LCD_void_sendString("Enter Password: ");
 
-	 // Get the user's input for the password using the keypad and display '*' characters
-	 for (u8 i = 0; i < 4; i++) {
-		 u8 key = KEYPAD_getPressedKey();
-		 enteredPassword = enteredPassword * 10 + key;
-		 H_LCD_void_sendData('*');
-	 }
-	 
-	 H_LCD_void_clearScreen();
-	 
-	 // At this point, 'enteredID' and 'enteredPassword' variables hold the user's input.
-	 // You can compare these values with the database as shown in the previous response.
-	 // The following code assumes that you have a database as mentioned earlier.
+	// Get the user's input for the password using the keypad and display '*' characters
+	for (u8 i = 0; i < 4; i++) {
+		u8 key = KEYPAD_getPressedKey();
+		enteredPassword = enteredPassword * 10 + key;
+		H_LCD_void_sendData('*');
+	}
+	
+	H_LCD_void_clearScreen();
+	
+	// At this point, 'enteredID' and 'enteredPassword' variables hold the user's input.
+	// You can compare these values with the database as shown in the previous response.
+	// The following code assumes that you have a database as mentioned earlier.
 
-	 int found = 0;
-	 u8 i;
-	 for (u8 i = 0; i < databaseSize; i++) {
-		 if (enteredID == IDs[i] && enteredPassword == passwords[i]) {
-			 found = 1;
-			 break;
-		 }
-	 }
+	int found = 0;
+	u8 i;
+	for (u8 i = 0; i < databaseSize; i++) {
+		if (enteredID == IDs[i] && enteredPassword == passwords[i]) {
+			found = 1;
+			break;
+		}
+	}
 
-	 // Display appropriate messages based on the comparison results
-	 if (found) {
-		 H_LCD_void_sendString("Welcome, ");
-		 H_LCD_void_sendString(names[i]);
-		 } else {
-		  // Check if the ID is incorrect
-		  int incorrectID = 1;
-		  for (u8 i = 0; i < databaseSize; i++) {
-			  if (enteredID == IDs[i]) {
-				  incorrectID = 0;
-				  break;
-			  }
-		  }
+	// Display appropriate messages based on the comparison results
+	if (found) {
+		H_LCD_void_sendString("Welcome, ");
+		H_LCD_void_sendString(names[i]);
+		} else {
+		// Check if the ID is incorrect
+		int incorrectID = 1;
+		for (u8 i = 0; i < databaseSize; i++) {
+			if (enteredID == IDs[i]) {
+				incorrectID = 0;
+				break;
+			}
+		}
 
-		  if (incorrectID) {
-			  H_LCD_void_sendString("Wrong ID");
-			  } else {
-			  H_LCD_void_sendString("Wrong Password");
-		  }
-	 }
+		if (incorrectID) {
+			H_LCD_void_sendString("Wrong ID");
+			} else {
+			H_LCD_void_sendString("Wrong Password");
+		}
+	}
 
 
 	while (1) {
 		
 		
-						
+		
 	}
 
 	return 0;
