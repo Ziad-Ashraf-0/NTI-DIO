b23ba67b8c6d03d8d342aa6bee05e05d0d272474
Ziad Ashraf <zashraf011@gmail.com>
2023-11-08 22:18:10 +0200

    os-project done

--- OS/OS.c
+++ OS/OS.c
@@ -1,9 +1,3 @@
-/*
-* OS.c
-*
-* Created: 10/31/2023 7:32:14 PM
-*  Author: zashr
-*/
 #include "../LIB/STD_TYPES.h"
 #include "OS.h"
 #include "../MCAL/TIMER0/TIMER0_interface.h"
@@ -11,9 +5,8 @@
 #include "../LIB/BIT_MATH.h"
 
 
+void runHighestPriorityTask() ;
 void pendSV(void);
-void runHighestPriorityTask();
-
 // Define the task functions
 void Task1Callback(void) {
 	// Toggle PB0 LED
@@ -27,7 +20,7 @@ void Task2Callback(void) {
 	DIO_Config led = {DIO_PORTB, DIO_PIN1};
 	DIO_U8GetPinValue(&led, &value);
 	if (value == 0) {
-		led.pin= DIO_PIN2;
+		led.pin = DIO_PIN2;
 		DIO_U8TogglePin(&led);
 	}
 }
@@ -40,29 +33,29 @@ void Task3Callback(void) {
 
 // Define the task control blocks
 TaskControlBlock tasks[] = {
-	{Task1Callback, 1000, 0}, // Task 1 with a 1-second period
-	{Task2Callback, 500, 0},  // Task 2 with a 500 ms period
-	{Task3Callback, 3000, 0}, // Task 3 with a 3-second period
+	{Task1Callback, 1000,0 , 2}, // Task 1 with a 1-second period
+	{Task2Callback, 500, 0, 1},  // Task 2 with a 500 ms period
+	{Task3Callback, 3000,0 ,3}, // Task 3 with a 3-second period
 };
-#define NUM_TASKS 5
+#define NUM_TASKS 3
 
-// Define a simple task queue using FIFO
-TaskControlBlock* taskQueue[5];
+// Define a simple task queue using priority
+TaskControlBlock* taskQueue[NUM_TASKS];
 u8 queueSize = 0;
-u8 queueFront = 0; // Front of the queue
-u8 queueRear = 0;  // Rear of the queue
 
 // Initialize the hardware
 void startOS() {
 	DIO_Config pins[] = {
-		{DIO_PORTB, DIO_PIN0, DIO_PIN_OUTPUT},
-		{DIO_PORTB, DIO_PIN1, DIO_PIN_INPUT},
-		{DIO_PORTB, DIO_PIN2, DIO_PIN_OUTPUT},
-		{DIO_PORTB, DIO_PIN3, DIO_PIN_OUTPUT},
+		{DIO_PORTB, DIO_PIN0, DIO_PIN_OUTPUT,DIO_PIN_LOW},
+		{DIO_PORTB, DIO_PIN1, DIO_PIN_INPUT,DIO_PIN_HIGH},
+		{DIO_PORTB, DIO_PIN2, DIO_PIN_OUTPUT,DIO_PIN_LOW},
+		{DIO_PORTB, DIO_PIN3, DIO_PIN_OUTPUT,DIO_PIN_LOW},
 	};
+
 	// Initialize pins
 	for (u8 i = 0; i < 4; i++) {
 		DIO_U8SetPinDirection(&pins[i]);
+		DIO_U8SetPinValue(&pins[i]);
 	}
 
 	Timer0_Config timer = {NORMAL, PRESCALER_1024};
@@ -74,30 +67,40 @@ void startOS() {
 }
 
 void pendSV(void) {
-	for (u8 i = 0; i < NUM_TASKS; i++) {
-		tasks[i].counter += 500; // Increment the counter for all tasks
+	  for (u8 i = 0; i < NUM_TASKS; i++) {
+		  tasks[i].counter += 500; // Increment the counter for all tasks
 
-		if (tasks[i].counter >= tasks[i].period) {
-			// Add the task to the task queue (FIFO)
-			if (queueSize < NUM_TASKS) {
-				taskQueue[queueRear] = &tasks[i];
-				queueRear = (queueRear + 1) % NUM_TASKS; // Circular queue
-				queueSize++;
-			}
-			tasks[i].counter = 0; // Reset the counter
-		}
-	}
-	runHighestPriorityTask(); // Execute the highest-priority task
+		  if (tasks[i].counter >= tasks[i].period) {
+			  // Task is ready to run, add it to the queue based on priority
+			  if (queueSize < NUM_TASKS) {
+				  u8 insertIndex = 0;
+				  for (u8 j = 0; j < queueSize; j++) {
+					  if (tasks[i].priority > taskQueue[j]->priority) {
+						  break;
+					  }
+					  insertIndex++;
+				  }
+				  // Shift existing tasks to make space for the new task
+				  for (u8 j = queueSize; j > insertIndex; j--) {
+					  taskQueue[j] = taskQueue[j - 1];
+				  }
+				  taskQueue[insertIndex] = &tasks[i];
+				  queueSize++;
+			  }else{
+				  	  // Clear the task queue before adding tasks
+				  	  queueSize = 0;
+			  }
+			 
+		  }
+	  }
+	  runHighestPriorityTask(); // Execute the highest-priority task
 }
 
-// Execute the next task from the FIFO queue
 void runHighestPriorityTask() {
-	if (queueSize > 0) {
-		TaskControlBlock* nextTask = taskQueue[queueFront];
-		queueFront = (queueFront + 1) % NUM_TASKS; // Circular queue
-		queueSize--;
-
-		// Execute the next task in the queue
-		nextTask->taskFunc();
+	for (u8 i = 0; i < NUM_TASKS; i++) {
+		if (tasks[i].counter >= tasks[i].period) {
+			tasks[i].counter = 0;
+			tasks[i].taskFunc();
+		}
 	}
 }
